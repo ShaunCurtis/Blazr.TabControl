ef62693cb094746d750303360e5665454f3c9d79
Shaun Curtis <shaun@coldelm.com>
2023-03-17 23:08:20 +0000

    Add project files.

---
+++ Blazr.TabControl/Components/BlazrTab.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazr.TabControl.Components;
+
+public class BlazrTab : ComponentBase
+{
+    [CascadingParameter] private Action<BlazrTab>? Register { get; set; }
+    [Parameter, EditorRequired] public string Label { get; set; } = "No Label Set";
+    [Parameter] public string Id { get; set; } = Guid.NewGuid().ToString();
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    // Registers the component on initialization
+    protected override void OnInitialized()
+        => this.Register?.Invoke(this);
+}
